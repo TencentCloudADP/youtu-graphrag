668762a7515d5cbe263b78b4afcf7a2f5e397fea
TencentCloudADP-DevRel <adp_devrel@tencent.com>
2025-10-20 17:02:30 +0800

    Revert "docs: deploy documentation on vercel"

--- docs/components/docs/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './PictureInPicture';
-export * from './GiscusComment';
-export { default as ClientComments } from './ClientComments';
-export { default as AuthorInfo, type AuthorInfoProps } from './AuthorInfo';
-export { default as CustomDocsPage, type CustomDocsPageProps } from './CustomDocsPage';
